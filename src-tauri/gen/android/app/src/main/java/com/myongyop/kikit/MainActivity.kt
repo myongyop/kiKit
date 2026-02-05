4922dec6dc12b22f82e12c256b90c7fbbfa019e6
myongyop <myongyop.lee@gmail.com>
2026-02-05 18:41:52 +0900

    Fix Android build config: update bundle identifier and add tauri script

---
+++ src-tauri/gen/android/app/src/main/java/com/myongyop/kikit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.myongyop.kikit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
